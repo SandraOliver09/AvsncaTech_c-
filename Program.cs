b270858f05387696165b9ef7584bea6fa41719bc
Sandraoliver <sandraolivertj09@gmail.com>
2026-04-13 11:49:49 -0300

    Aula1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+Console.WriteLine("Hello, World!");
+
+string nome = " Sandra ";
+
+Console.WriteLine(nome);
+
+Console.WriteLine("Digite seu sobrenome");
+
+string sobrenome = Console.ReadLine();
+
+Console.WriteLine(" Olá," + nome +sobrenome+ "!!!");
+
+int idade = 23;
+
+Boolean usuarioLogado = true;
+
+float altura = 1.63f;
+
+Console.WriteLine (usuarioLogado);
+
+    Console.WriteLine( "Bem Vindo! ");
+
+    Console.WriteLine(" Faça o login ");
+
+    Console.WriteLine(" Minha altura é "  + altura);
+    Console.WriteLine(" Minha idade é "  + idade);
+
+
+
+
+
+
